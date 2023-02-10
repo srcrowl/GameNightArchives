eb80a82992e7e322815a1d906f86561193b81cc9
srcrowl <sc8wf@virginia.edu>
2023-02-10 15:48:10 -0500

    updates to prediction page

--- pages/6_Predict_Outcomes.py
+++ pages/6_Predict_Outcomes.py
@@ -16,42 +16,68 @@ bggtype_list = gplayed_dict['BGG Type'][gplayed_dict['BGG Type'] >= min_games].i
 bggcat_list = gplayed_dict['BGG Category'][gplayed_dict['BGG Category'] >= min_games].index.values
 bggmech_list = gplayed_dict['BGG Mechanism'][gplayed_dict['BGG Mechanism'] >= min_games].index.values
 
+#decide which categories to include
+cols = st.columns(4)
+use_owner = cols[0].checkbox('Game Owner', value = True)
+use_format = cols[1].checkbox('Game Format', value = True)
+use_type = cols[2].checkbox('Game Type', value = True)
+use_theme = cols[3].checkbox('Game Theme', value = True)
+cols = st.columns(3)
+use_bggtype = cols[0].checkbox('BGG Type', value = True)
+use_bggcat = cols[1].checkbox('BGG Category', value = True)
+use_bggmech = cols[2].checkbox('BGG Mechanism', value = True)
+
+
 generate_classifier = st.checkbox('Build Naive Bayes Classifier', value = False)
 if generate_classifier:
     data = st.session_state['Full Data'].copy()
     data['Winner'] = data['Winner'].apply(lambda x: x.split(';'))
     data = data.explode('Winner').reset_index()
 
-
-    owner = st.session_state['Owner'].copy()
-    owner = owner[owner['Game Owner'].isin(owner_list)]
-    owner['Game Owner'] = owner['Game Owner'].apply(lambda x: 'Owner:'+x)
-    owner = owner.rename({'Game Owner': 'Category'}, axis = 1)
-    gformat = st.session_state['Format'].copy()
-    gformat = gformat[gformat['Game Format'].isin(format_list)]
-    gformat['Game Format'] = gformat['Game Format'].apply(lambda x: 'Format:'+x)
-    gformat = gformat.rename({'Game Format': 'Category'}, axis = 1)
-    gtype = st.session_state['Type'].copy()
-    gtype = gtype[gtype['Game Type'].isin(type_list)]
-    gtype['Game Type'] = gtype['Game Type'].apply(lambda x: 'Type:'+x)
-    gtype = gtype.rename({'Game Type': 'Category'}, axis = 1)
-    gtheme = st.session_state['Theme'].copy()
-    gtheme = gtheme[gtheme['Theme'].isin(theme_list)]
-    gtheme['Theme'] = gtheme['Theme'].apply(lambda x: 'Theme:'+x)
-    gtheme = gtheme.rename({'Theme': 'Category'}, axis = 1)
-    bgg_type = st.session_state['BGG Type'].copy()
-    bgg_type = bgg_type[bgg_type['BGG Type'].isin(bggtype_list)]
-    bgg_type['BGG Type'] = bgg_type['BGG Type'].apply(lambda x: 'BGG_Type:'+x)
-    bgg_type = bgg_type.rename({'BGG Type': 'Category'}, axis = 1)
-    bgg_cat = st.session_state['BGG Category'].copy()
-    bgg_cat = bgg_cat[bgg_cat['BGG Category'].isin(bggcat_list)]
-    bgg_cat['BGG Category'] = bgg_cat['BGG Category'].apply(lambda x: 'BGG_Cat:'+x)
-    bgg_cat = bgg_cat.rename({'BGG Category': 'Category'}, axis = 1)
-    bgg_mech = st.session_state['BGG Mechanism'].copy()
-    bgg_mech = bgg_mech[bgg_mech['BGG Mechanism'].isin(bggmech_list)]
-    bgg_mech['BGG Mechanism'] = bgg_mech['BGG Mechanism'].apply(lambda x: 'BGG_Mech:'+x)
-    bgg_mech = bgg_mech.rename({'BGG Mechanism': 'Category'}, axis = 1)
-    groupings = pd.concat([owner, gformat, gtype, gtheme, bgg_cat, bgg_mech, bgg_type])
+    groupings = []
+    if use_owner:
+        owner = st.session_state['Owner'].copy()
+        owner = owner[owner['Game Owner'].isin(owner_list)]
+        owner['Game Owner'] = owner['Game Owner'].apply(lambda x: 'Owner:'+x)
+        owner = owner.rename({'Game Owner': 'Category'}, axis = 1)
+        groupings.append(owner)
+    if use_format:
+        gformat = st.session_state['Format'].copy()
+        gformat = gformat[gformat['Game Format'].isin(format_list)]
+        gformat['Game Format'] = gformat['Game Format'].apply(lambda x: 'Format:'+x)
+        gformat = gformat.rename({'Game Format': 'Category'}, axis = 1)
+        groupings.append(gformat)
+    if use_type:
+        gtype = st.session_state['Type'].copy()
+        gtype = gtype[gtype['Game Type'].isin(type_list)]
+        gtype['Game Type'] = gtype['Game Type'].apply(lambda x: 'Type:'+x)
+        gtype = gtype.rename({'Game Type': 'Category'}, axis = 1)
+        groupings.append(gtype)
+    if use_theme:
+        gtheme = st.session_state['Theme'].copy()
+        gtheme = gtheme[gtheme['Theme'].isin(theme_list)]
+        gtheme['Theme'] = gtheme['Theme'].apply(lambda x: 'Theme:'+x)
+        gtheme = gtheme.rename({'Theme': 'Category'}, axis = 1)
+        groupings.append(gtheme)
+    if use_bggtype:
+        bgg_type = st.session_state['BGG Type'].copy()
+        bgg_type = bgg_type[bgg_type['BGG Type'].isin(bggtype_list)]
+        bgg_type['BGG Type'] = bgg_type['BGG Type'].apply(lambda x: 'BGG_Type:'+x)
+        bgg_type = bgg_type.rename({'BGG Type': 'Category'}, axis = 1)
+        groupings.append(bgg_type)
+    if use_bggcat:
+        bgg_cat = st.session_state['BGG Category'].copy()
+        bgg_cat = bgg_cat[bgg_cat['BGG Category'].isin(bggcat_list)]
+        bgg_cat['BGG Category'] = bgg_cat['BGG Category'].apply(lambda x: 'BGG_Cat:'+x)
+        bgg_cat = bgg_cat.rename({'BGG Category': 'Category'}, axis = 1)
+        groupings.append(bgg_cat)
+    if use_bggmech:
+        bgg_mech = st.session_state['BGG Mechanism'].copy()
+        bgg_mech = bgg_mech[bgg_mech['BGG Mechanism'].isin(bggmech_list)]
+        bgg_mech['BGG Mechanism'] = bgg_mech['BGG Mechanism'].apply(lambda x: 'BGG_Mech:'+x)
+        bgg_mech = bgg_mech.rename({'BGG Mechanism': 'Category'}, axis = 1)
+        groupings.append(bgg_mech)
+    groupings = pd.concat(groupings)
 
 
 
@@ -115,6 +141,10 @@ if generate_classifier:
     cols[0].metric('Sam', f'{round(probs[0][0]*100, 2)}%')
     cols[1].metric('Gabi', f'{round(probs[0][1]*100, 2)}%')
     cols[2].metric('Reagan', f'{round(probs[0][2]*100, 2)}%')
+    #how game is classified
+    st.markdown(f'__Labels associated with {game} used for prediction__')
+    st.write(game_cats)
+
     
     
     
